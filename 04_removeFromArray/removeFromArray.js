68fa2692edd25b527e930b2d14a8830194c4627a
Rishabh <rishabh4883@gmail.com>
2022-07-22 15:21:54 +0530

    Complete first 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for(let i=0; i<args.length; i++)
+    {
+        if(arr.indexOf(args[i]) < 0) continue;
+        arr.splice(arr.indexOf(args[i]), 1)
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let low;
+    let high;
+    if(a<0 || b<0 ||  !Number.isInteger(a) || !Number.isInteger(b) ) return "ERROR";
+    if(a < b) { low = a; high = b; }
+    else  { low = b; high = a; }
 
+    for(let i=low; i<=high; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
